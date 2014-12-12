681a8a2f5b5be685283b9824b39f21425d182aa9
rolandoh <rolandoh@unitec.edu>
2014-12-12 08:55:37 -0600

    Clase Equipo Creada

---
+++ src/Logica/Equipo.java
@@ -0,0 +1,25 @@
+/*
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
+package Logica;
+
+/**
+ *
+ * @author Rolando
+ */
+public class Equipo {
+    private Jugador jugadores[] = new Jugador[23];
+    private int contador;
+    
+    /* private boolean searchCamisa(int camias){
+    for(int i=0; i<contador; i++ ){
+    if(jugadores[i])
+    }
+    }*/
+    
+    public boolean addJugador(int numCamisa, String nombre, String posicion){
+        return false;
+    }
+}
